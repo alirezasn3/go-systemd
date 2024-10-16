d025fe4522dd8431d0929d372cdd505e22197c3b
Alireza Saadatnia <alirezasn3@gmail.com>
2024-10-16 21:39:36 +0330

    added empty functions to avoid build errors on windows and darwin

--- systemd_darwin.go
+++ systemd_darwin.go
@@ -1,1 +1,18 @@
-package goSystemd
+package gosystemd
+
+type Service struct {
+	Name       string
+	ExecStart  string
+	Restart    string
+	RestartSec string
+}
+
+// this function only work on linux
+func CreateService(service *Service) error {
+	return nil
+}
+
+// this function only work on linux
+func DeleteService(serviceName string) error {
+	return nil
+}
